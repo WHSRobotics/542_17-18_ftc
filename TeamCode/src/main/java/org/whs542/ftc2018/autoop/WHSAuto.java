0f06547dcdfb20e78a5cba70e0e43c79686ad70e
Jason Jewik <jj11d7t@gmail.com>
2017-11-03 17:12:39 -0700

    AutoOp State Machine changes

--- TeamCode/src/main/java/org/whs542/ftc2018/autoop/WHSAuto.java
+++ TeamCode/src/main/java/org/whs542/ftc2018/autoop/WHSAuto.java
@@ -8,7 +8,7 @@ import org.whs542.util.Timer;
 import org.whs542.util.Position;
 import org.whs542.util.Functions;
 import org.whs542.util.Coordinate;
-import org.whs542.util.Toggler
+import org.whs542.util.Toggler;
 
 /**
  * Created by Jason on 11/03/2017.
@@ -27,61 +27,70 @@ public class WHSAuto extends OpMode {
 
     //States
     enum STATES {
-        EXTEND_JEWEL_ARM, WAIT_FOR_COLOR, HIT_JEWEL, RETRACT_JEWEL_ARM, DRIVE_OFF_PLATFORM, DRIVE_TO_BOX, PLACE_GLYPH, PARK
+        INIT, HIT_JEWEL, DRIVE_INTO_SAFEZONE, DRIVE_TO_BOX, PLACE_GLYPH, END;
+        public static STATES[] vals = values();
+        public STATES next() {
+            return vals[(this.ordinal()+1) % vals.length];
+        }
     }
     STATES currentState;
     String currentStateDesc;
+    static final int NUM_OF_STATES = STATES.values().length;
+
+    boolean[] stateEnabled = new boolean[NUM_OF_STATES + 1];
+
+    public void defineStateEnabled() {
+        stateEnabled[STATES.INIT.ordinal()] = true;
+        stateEnabled[STATES.HIT_JEWEL.ordinal()] = true;
+        stateEnabled[STATES.DRIVE_INTO_SAFEZONE.ordinal()] = true;
+        stateEnabled[STATES.DRIVE_TO_BOX.ordinal()] = true;
+        stateEnabled[STATES.PLACE_GLYPH.ordinal()] = true;
+        stateEnabled[STATES.END.ordinal()] = true;
+    }
 
     @Override
     public void init() {
+        robot = new WHSRobotImpl(hardwareMap);
+        currentState = STATES.INIT; //set the initial state
 
+        telemetry.setMsTransmissionInterval(50); //set driver station update frequency
+        telemetry.log().setCapacity(6); //set max number of lines logged by telemetry
     }
 
     @Override
     public void loop() {
 
+        robot.estimateHeading();
+        robot.estimatePosition();
+
         //State Machine
         switch (currentState) {
-            case EXTEND_JEWEL_ARM:
-                currentState = STATES.WAIT_FOR_COLOR;
-                currentStateDesc = "extending jewel arm";
-                break;
-            case WAIT_FOR_COLOR:
-                currentState = STATES.HIT_JEWEL;
-                currentStateDesc = "waiting for color sensor";
+            case INIT:
+                currentStateDesc = "beginning AutoOp";
+                currentState = STATES.next();
                 break;
             case HIT_JEWEL:
-                currentState = STATES.RETRACT_JEWEL_ARM;
                 currentStateDesc = "slapping jewel";
+                currentState = STATES.DRIVE_INTO_SAFEZONE;
                 break;
-            case RETRACT_JEWEL_ARM:
-                currentState = STATES.DRIVE_OFF_PLATFORM;
-                currentStateDesc = "retracting jewel arm";
-                break;
-            case DRIVE_OFF_PLATFORM:
-                currentState = STATES.DRIVE_TO_BOX;
+            case DRIVE_INTO_SAFEZONE:
                 currentStateDesc = "driving off platform";
+                currentState = STATES.DRIVE_TO_BOX;
                 break;
             case DRIVE_TO_BOX:
-                currentState = STATES.PLACE_GLYPH;
                 currentStateDesc = "driving to box while scanning target";
+                currentState = STATES.PLACE_GLYPH;
                 break;
             case PLACE_GLYPH:
-                currentState = STATES.PARK;
                 currentStateDesc = "moving glyph";
+                currentState = STATES.END;
                 break;
-            case PARK:
-                currentStateDesc = "parking, hooray we made it!";
+            case END:
+                currentStateDesc = "hooray we made it!";
             default: break;
         }
 
         //Logging the current state
         telemetry.log().add(currentStateDesc);
     }
-
-    @Override
-    public void stop() {
-
-    }
-
 }
