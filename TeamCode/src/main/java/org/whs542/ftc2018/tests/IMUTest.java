c4305de61b2b9e70c367b20f3c7104b73af88d9d
Jason Jewik <jj11d7t@gmail.com>
2017-11-17 17:09:27 -0800

    IMU debugging changes

--- TeamCode/src/main/java/org/whs542/ftc2018/tests/IMUTest.java
+++ TeamCode/src/main/java/org/whs542/ftc2018/tests/IMUTest.java
@@ -1,5 +1,6 @@
 package org.whs542.ftc2018.tests;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.whs542.ftc2018.subsys.IMU;
@@ -9,37 +10,30 @@ import org.whs542.subsys.drivetrain.TankDrivetrain;
 /**
  * Created by ivanm on 11/4/2017.
  */
-
+@Autonomous(name = "IMUTest", group = "tests")
 public class IMUTest extends OpMode {
 
     IMU imu;
-    TankDrivetrain drivetrain;
 
     @Override
     public void init() {
         imu = new IMU(hardwareMap);
-        drivetrain = new TileRunner(hardwareMap);
     }
 
     @Override
     public void loop() {
-        drivetrain.operate(0, 0);
 
         double heading = imu.getHeading();
 
         double[] threeHeading = imu.getThreeHeading();
 
-        double accel = imu.getAccelerationMag();
 
-        String headingValue = String.valueOf(heading);
-        String accelValue = String.valueOf(accel);
 
-        telemetry.addData("Heading: ", headingValue);
-        telemetry.addData("Acceleration: ", accelValue);
+        telemetry.addData("Heading: ", heading);
 
-        if(gamepad1.x){
-            //imu.calibrateHeading();
-        }
+       /* if(gamepad1.x){
+            imu.();
+        }*/
 
         telemetry.addData("x", threeHeading[0]);
         telemetry.addData("y", threeHeading[1]);
