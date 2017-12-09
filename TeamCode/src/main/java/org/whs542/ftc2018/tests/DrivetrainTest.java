ca39cd212c8a0995aeb48b09254e4937714be997
Jason Jewik <jj11d7t@gmail.com>
2017-12-08 16:00:20 -0800

    Updates to drivetraintest

--- TeamCode/src/main/java/org/whs542/ftc2018/tests/DrivetrainTest.java
+++ TeamCode/src/main/java/org/whs542/ftc2018/tests/DrivetrainTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.whs542.ftc2018.subsys.TileRunner;
 import org.whs542.subsys.drivetrain.TankDrivetrain;
+import org.whs542.util.Toggler;
 
 /**
  * Created by Amar2 on 10/21/2017.
@@ -12,7 +13,7 @@ import org.whs542.subsys.drivetrain.TankDrivetrain;
 @TeleOp(name = "drivetrainTest", group = "tests")
 public class DrivetrainTest extends OpMode {
 
-    TileRunner drivetrain;
+    /*TileRunner drivetrain;
 
     @Override
     public void init() {
@@ -44,6 +45,51 @@ public class DrivetrainTest extends OpMode {
         }
         else if(gamepad1.a){
             drivetrain.backRight.setPower(0.5);
-        }*/
+        }
+    }*/
+
+    TileRunner drivetrain;
+    Toggler scaleTog = new Toggler(3);
+    String mode = "";
+    double[] power = new double[2];
+    Toggler powerTog = new Toggler(50);
+
+    @Override
+    public void init() {
+        drivetrain = new TileRunner(hardwareMap);
+        telemetry.log().add("mode (scaled/normal) switch : gamepad1-x");
+        telemetry.log().add("orientation switch : gamepad1-a");
+    }
+
+    @Override
+    public void loop() {
+
+        scaleTog.changeState(gamepad1.x);
+        drivetrain.switchOrientation(gamepad1.a);
+        powerTog.changeState(gamepad1.dpad_up, gamepad1.dpad_down);
+
+        if (scaleTog.currentState() == 0){
+            drivetrain.operate(gamepad1.left_stick_y, gamepad1.right_stick_y);
+            mode = "Normal";
+        } else if (scaleTog.currentState() == 1){
+            drivetrain.operateWithOrientationScaled(gamepad1.left_stick_y, gamepad1.right_stick_y);
+            mode = "Scaled";
+        } else {
+            if (gamepad1.b) {
+                drivetrain.operate(powerTog.currentState() * 0.02, powerTog.currentState() * 0.02);
+            } else {
+                drivetrain.operate(0.0, 0.0);
+            }
+            mode = "Step";
+        }
+
+
+        telemetry.addData("Mode:", mode);
+        telemetry.addData("Orientation:", drivetrain.getOrientation());
+        telemetry.addData("LeftStickY:", gamepad1.left_stick_y);
+        telemetry.addData("RightStickY:", gamepad1.right_stick_y);
+        //telemetry.addData("Scaled L", drivetrain.getScaledPower(gamepad1.left_stick_y));
+        //telemetry.addData("Scaled R", drivetrain.getScaledPower(gamepad1.right_stick_y));
+        telemetry.addData("Power (step)", powerTog.currentState()*0.02);
     }
 }
